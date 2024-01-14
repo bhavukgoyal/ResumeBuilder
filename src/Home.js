b2dd387bbe293f6e2e299cf9596c3d4a5f261194
Bhavuk Goyal <goyalbhavuk19@gmail.com>
2024-01-14 20:17:37 +0530

    commit

--- src/Home.js
+++ src/Home.js
@@ -23,7 +23,7 @@ function Home() {
     setTemplate(templateNumber);
     
       // Navigate to the desired link using window.location
-      window.location.href = 'https://bhavukgoyal.github.io/Personal%20Info';
+      window.location.href = './Personal Info';
     
   }
   useEffect(() => {
